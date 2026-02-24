3df1212e4c16c92010689d1835fb9958e9689b35
PuntunzOfficial <64560568+PuntunzOfficial@users.noreply.github.com>
2026-02-24 01:09:53 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -88,4 +88,4 @@ sleep 15
 konsole --new-tab &
 
 # 2. Open a terminal running mpv with ASCII video
-konsole --new-tab -e bash -c "mpv --vo=tct '$SCRIPT_DIR/wallpaper.mp4'; exec bash" &
+mpv --no-config --vo=tct --really-quiet --loop "$SCRIPT_DIR/wallpaper.mp4"
