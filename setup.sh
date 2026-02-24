31d26196b49a075f5676398773da9ad476d70949
PuntunzOfficial <64560568+PuntunzOfficial@users.noreply.github.com>
2026-02-24 01:32:58 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -111,7 +111,7 @@ fi
 # Adjust "konsole" to "gnome-terminal", "alacritty", or "kitty" if needed
 
 # 1. Open an empty terminal
-konsole --new-tab &
+konsole --new-tab --geometry 80x24+100+50 &&
 
 # 2. Open a terminal running mpv with ASCII video
 mpv --no-config --vo=tct --really-quiet --loop "$SCRIPT_DIR/wallpaper.mp4"
