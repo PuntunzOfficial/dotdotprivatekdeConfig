6dc9d4304ea10496641398cbc59b5e5519eab5a6
PuntunzOfficial <64560568+PuntunzOfficial@users.noreply.github.com>
2026-02-25 00:21:27 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -114,8 +114,7 @@ fi
 konsole --new-tab &
 
 
-konsole --new-window --hold --maximize -e bash -c "mpv --no-config --vo=tct --really-quiet --loop --fullscreen '$SCRIPT_DIR/wallpaper.mp4'; exec bash" &
-
+mpv --no-config --vo=tct --really-quiet --loop --fullscreen "$SCRIPT_DIR/wallpaper.mp4"
 
 
 #this option is good, opens new window
